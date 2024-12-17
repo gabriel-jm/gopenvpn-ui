1c886b1abb7d70cf6d678f1dfb29696527d3f457
Gabriel José <gabriel.josemelo@hotmail.com>
2024-12-17 17:56:12 -0300

    feat: update profile request

--- internal/connections/connections.go
+++ internal/connections/connections.go
@@ -6,59 +6,72 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"time"
 )
 
 func StablishConnection(address, username, password string) error {
-	err := downloadProfile(address, username, password)
+	_, err := downloadProfile(address, username, password)
+
+	log.Println(err)
 
 	if err != nil {
-		return nil
+		return err
 	}
 
 	return nil
 }
 
-func downloadProfile(address, username, password string) error {
-	url := fmt.Sprintf("http://%s/rest/GetUserlogin", address)
+func downloadProfile(address, username, password string) (string, error) {
+	rawUrl, err := url.Parse(address)
+
+	if err != nil {
+		log.Println("[Download Profile] Error on Address Parsing")
+		return "", err
+	}
+
+	url := fmt.Sprintf("https://%s/rest/GetUserlogin", rawUrl.Host)
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 
 	if err != nil {
 		log.Println("[Download Profile] Error creating request")
-		return err
+		return "", err
 	}
 
 	basicAuth := base64.StdEncoding.EncodeToString([]byte(username + ":" + password))
-	req.Header.Set("Authentication", "Basic "+basicAuth)
+	req.Header.Set("Authorization", "Basic "+basicAuth)
 
 	res, err := http.DefaultClient.Do(req)
 
 	if err != nil {
-		log.Println("[Download Profile] Error on profile download")
-		return err
+		log.Println("[Download Profile] Error on download request")
+		return "", err
 	}
 
 	resBody, err := io.ReadAll(res.Body)
 
 	if err != nil {
 		log.Println("[Download Profile] Error on request body reading")
-		return err
+		return "", err
 	}
 
 	if res.StatusCode > 299 {
-		return fmt.Errorf(
+		return "", fmt.Errorf(
 			"[Download Profile] Request error. status: %d, body: %s",
 			res.StatusCode,
 			resBody,
 		)
 	}
 
-	err = os.WriteFile("./profiles/profile", resBody, 0644)
+	fileName := time.Now().String() + "-profile.ovpn"
+	filePath := "./profiles/" + fileName
+	err = os.WriteFile(filePath, resBody, 0644)
 
 	if err != nil {
 		log.Println("[Download Profile] Error writing file")
-		return err
+		return "", err
 	}
 
-	return nil
+	return fileName, nil
 }
